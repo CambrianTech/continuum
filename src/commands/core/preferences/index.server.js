f7b4d17b65eb8475fff112d026b9e7fcd19d32c7
joelteply <joelteply@yahoo.com>
2025-06-29 16:40:12 -0500

    substantial error progress

---
+++ src/commands/core/preferences/index.server.js
@@ -0,0 +1,7 @@
+/**
+ * Preferences Command Module - Server Export
+ * Pure ES module export for Node.js daemon system
+ */
+
+export { PreferencesCommand } from './PreferencesCommand.js';
+export { PreferencesCommand as default } from './PreferencesCommand.js';
---
+++ src/commands/core/reload/index.server.js
@@ -0,0 +1,7 @@
+/**
+ * Reload Command Module - Server Export
+ * Pure ES module export for Node.js daemon system
+ */
+
+export { ReloadCommand } from './ReloadCommand.js';
+export { ReloadCommand as default } from './ReloadCommand.js';
