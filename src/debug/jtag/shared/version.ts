22331200e6f0b00efeeb395ee43f5d0958869c43
joelteply <joelteply@yahoo.com>
2025-10-21 14:54:28 -0500

    just validation

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-10-21T18:08:59.107Z
+ * Generated on 2025-10-21T19:37:51.330Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.3640';
+export const VERSION = '1.0.3642';
 export const PACKAGE_NAME = '@continuum/jtag';
