8243732bf89ba88b6c9fd8ea7e27802c87433b79
joelteply <joelteply@yahoo.com>
2025-10-29 21:38:52 -0500

    all fine tuning adapters set up

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-10-30T02:07:32.400Z
+ * Generated on 2025-10-30T02:35:45.710Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.3816';
+export const VERSION = '1.0.3824';
 export const PACKAGE_NAME = '@continuum/jtag';
