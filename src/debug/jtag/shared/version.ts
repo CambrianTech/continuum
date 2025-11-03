865f156ac8bba45949fabda86306efe254fa39b4
joelteply <joelteply@yahoo.com>
2025-11-02 23:26:08 -0600

    bugs fixed

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-02T23:35:17.945Z
+ * Generated on 2025-11-03T05:21:51.928Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.3888';
+export const VERSION = '1.0.3892';
 export const PACKAGE_NAME = '@continuum/jtag';
