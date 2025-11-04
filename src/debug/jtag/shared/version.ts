0e38f44fb450531bd6a5f446bd2561df406190bf
joelteply <joelteply@yahoo.com>
2025-11-04 09:20:52 -0600

    decent design of user list widget

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-04T09:52:26.345Z
+ * Generated on 2025-11-04T14:05:48.611Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.3980';
+export const VERSION = '1.0.3989';
 export const PACKAGE_NAME = '@continuum/jtag';
