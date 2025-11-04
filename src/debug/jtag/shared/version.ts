8fb750fadb0589dd4c12a6a141c592b948bde034
joelteply <joelteply@yahoo.com>
2025-11-04 14:34:20 -0600

    slightly better desing

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-04T14:05:48.611Z
+ * Generated on 2025-11-04T20:24:47.509Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.3989';
+export const VERSION = '1.0.3993';
 export const PACKAGE_NAME = '@continuum/jtag';
