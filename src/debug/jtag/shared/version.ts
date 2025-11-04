3cc9958c92043d2b00d114a5a49e32596a4b8d1b
joelteply <joelteply@yahoo.com>
2025-11-04 15:03:00 -0600

    best appearance so far

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-04T20:49:23.925Z
+ * Generated on 2025-11-04T20:58:40.840Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.3997';
+export const VERSION = '1.0.3999';
 export const PACKAGE_NAME = '@continuum/jtag';
