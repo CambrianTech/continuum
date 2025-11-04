c88832a90a7bb8006d2c55ce989439c45e047e26
joelteply <joelteply@yahoo.com>
2025-11-04 17:42:47 -0600

    segmented control diamond and other layout done

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-04T23:11:17.169Z
+ * Generated on 2025-11-04T23:37:40.107Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.4005';
+export const VERSION = '1.0.4009';
 export const PACKAGE_NAME = '@continuum/jtag';
