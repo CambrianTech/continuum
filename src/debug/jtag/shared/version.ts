b5759094a3907d19acad8b21f78092ad5432a2ff
joelteply <joelteply@yahoo.com>
2025-11-04 14:51:43 -0600

    best ux and user list has looked

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-04T20:24:47.509Z
+ * Generated on 2025-11-04T20:49:23.925Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.3993';
+export const VERSION = '1.0.3997';
 export const PACKAGE_NAME = '@continuum/jtag';
