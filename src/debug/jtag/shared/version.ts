db2c607c7280614e33cae6ab448ffc6936fd27b9
joelteply <joelteply@yahoo.com>
2025-11-07 13:48:12 -0600

    New papers and documentation from conversation

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-07T05:46:17.208Z
+ * Generated on 2025-11-07T16:53:08.489Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.4194';
+export const VERSION = '1.0.4202';
 export const PACKAGE_NAME = '@continuum/jtag';
