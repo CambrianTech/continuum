5b5a131aea0f42691fd9faf046bd9a90c8f6e06c
joelteply <joelteply@yahoo.com>
2025-11-10 09:06:22 -0600

    docs for persona

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-10T05:07:46.603Z
+ * Generated on 2025-11-10T05:16:28.366Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.4332';
+export const VERSION = '1.0.4334';
 export const PACKAGE_NAME = '@continuum/jtag';
