1401173418adee48fabc85bfbd3ab1749b231756
joelteply <joelteply@yahoo.com>
2025-11-09 19:47:59 -0600

    fix chat css

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-09T13:04:09.759Z
+ * Generated on 2025-11-09T19:51:04.739Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.4298';
+export const VERSION = '1.0.4314';
 export const PACKAGE_NAME = '@continuum/jtag';
