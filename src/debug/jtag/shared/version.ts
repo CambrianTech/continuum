76c68e644f60371c952c2cb0f61f6ccbdcb48a28
joelteply <joelteply@yahoo.com>
2025-11-11 00:54:15 -0600

    decision entry for introspection

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-11T05:57:00.546Z
+ * Generated on 2025-11-11T06:50:29.405Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.4350';
+export const VERSION = '1.0.4356';
 export const PACKAGE_NAME = '@continuum/jtag';
