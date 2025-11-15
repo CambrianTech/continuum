4b4e80f996fb2ee1a2e0a22acec3a1f9cd03a974
joelteply <joelteply@yahoo.com>
2025-11-14 22:11:17 -0600

    user verified adapter cleanup

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-15T03:33:03.538Z
+ * Generated on 2025-11-15T04:08:31.971Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.4626';
+export const VERSION = '1.0.4632';
 export const PACKAGE_NAME = '@continuum/jtag';
