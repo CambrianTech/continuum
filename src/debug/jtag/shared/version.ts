29ec680491a70786402f286e26f4a64bf03b86f8
joelteply <joelteply@yahoo.com>
2025-11-25 21:33:59 -0500

    genome architecture for UI-UX use case

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-25T20:53:00.087Z
+ * Generated on 2025-11-26T01:38:21.923Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5355';
+export const VERSION = '1.0.5376';
 export const PACKAGE_NAME = '@continuum/jtag';
