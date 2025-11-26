9f28ab3d8b5b038eb6cc95c76f46128d23f8abbb
joelteply <joelteply@yahoo.com>
2025-11-26 13:44:02 -0500

    chat analysis commands
    Signed-off-by: joelteply <joelteply@yahoo.com>

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-26T07:09:12.915Z
+ * Generated on 2025-11-26T18:23:27.829Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5396';
+export const VERSION = '1.0.5414';
 export const PACKAGE_NAME = '@continuum/jtag';
