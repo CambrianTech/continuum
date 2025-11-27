ad6dfc8b247f613773c55d4b161d2fb47d55f7a5
joelteply <joelteply@yahoo.com>
2025-11-27 17:46:51 -0500

    emoter working ok

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-27T20:25:37.011Z
+ * Generated on 2025-11-27T22:36:56.625Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5466';
+export const VERSION = '1.0.5472';
 export const PACKAGE_NAME = '@continuum/jtag';
