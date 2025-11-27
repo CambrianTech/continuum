04703ae7a717265d366825c72f96e01c8e9c12e3
joelteply <joelteply@yahoo.com>
2025-11-27 14:37:45 -0500

    gen code

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-27T00:43:22.085Z
+ * Generated on 2025-11-27T19:33:17.253Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5430';
+export const VERSION = '1.0.5420';
 export const PACKAGE_NAME = '@continuum/jtag';
