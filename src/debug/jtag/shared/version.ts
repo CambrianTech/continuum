7efaff4388e38f6685b6561f08aaace9be4a26f0
joelteply <joelteply@yahoo.com>
2025-11-30 11:28:25 -0600

    massive file

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-30T16:33:55.144Z
+ * Generated on 2025-11-30T17:20:59.117Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5674';
+export const VERSION = '1.0.5676';
 export const PACKAGE_NAME = '@continuum/jtag';
