d4bce7db5f0298d29beb972da53b08456f94f69d
joelteply <joelteply@yahoo.com>
2025-12-01 01:44:17 -0600

    human verified reduced log spam

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-12-01T06:54:37.189Z
+ * Generated on 2025-12-01T07:37:56.139Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5685';
+export const VERSION = '1.0.5690';
 export const PACKAGE_NAME = '@continuum/jtag';
