d4578bd6e89ea3dcad745d16247e102f477018a9
joelteply <joelteply@yahoo.com>
2025-12-01 13:43:22 -0600

    got rid of cognition logs

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-12-01T18:57:25.441Z
+ * Generated on 2025-12-01T19:36:27.662Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5698';
+export const VERSION = '1.0.5699';
 export const PACKAGE_NAME = '@continuum/jtag';
