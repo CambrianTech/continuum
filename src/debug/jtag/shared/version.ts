e8d3f8c896d6bed67e2136e88d46f77699e47d7e
joelteply <joelteply@yahoo.com>
2025-11-30 18:12:13 -0600

    commit

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-11-30T23:08:28.263Z
+ * Generated on 2025-11-30T23:54:39.136Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5656';
+export const VERSION = '1.0.5662';
 export const PACKAGE_NAME = '@continuum/jtag';
