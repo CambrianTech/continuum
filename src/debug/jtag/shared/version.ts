e940be134593d835c877eab71f69ed6997b19dc9
joelteply <joelteply@yahoo.com>
2025-12-08 12:28:33 -0600

    fix import/export for generate

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-12-08T15:59:16.184Z
+ * Generated on 2025-12-08T18:23:09.462Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5892';
+export const VERSION = '1.0.5898';
 export const PACKAGE_NAME = '@continuum/jtag';
