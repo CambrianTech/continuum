2f3cbe1df66314277dea36f3d5b33715d61cb1f8
joelteply <joelteply@yahoo.com>
2025-12-11 04:57:57 -0600

    change image

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-12-11T07:43:28.920Z
+ * Generated on 2025-12-11T10:26:38.470Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5977';
+export const VERSION = '1.0.5980';
 export const PACKAGE_NAME = '@continuum/jtag';
