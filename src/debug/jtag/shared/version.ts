9d639c7d4711d1b20b8edc434c0c8beaaea34885
joelteply <joelteply@yahoo.com>
2025-12-11 01:33:31 -0600

    delete useless stuff
    tested live

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-12-11T06:43:47.331Z
+ * Generated on 2025-12-11T07:28:58.861Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.5974';
+export const VERSION = '1.0.5975';
 export const PACKAGE_NAME = '@continuum/jtag';
