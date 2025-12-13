ba9e25299bd70487628e949627f9335bbf230222
joelteply <joelteply@yahoo.com>
2025-12-13 14:31:11 -0600

    ts-rs version

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-12-13T20:12:13.293Z
+ * Generated on 2025-12-13T20:21:56.274Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.6000';
+export const VERSION = '1.0.6001';
 export const PACKAGE_NAME = '@continuum/jtag';
