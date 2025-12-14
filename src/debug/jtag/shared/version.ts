fe9460d6e6a9bf50c2bf07587d327c265b022be5
joelteply <joelteply@yahoo.com>
2025-12-13 19:53:18 -0600

    correct settings for workers

--- src/debug/jtag/shared/version.ts
+++ src/debug/jtag/shared/version.ts
@@ -1,8 +1,8 @@
 /**
  * Version Constants - Auto-generated from package.json
- * Generated on 2025-12-13T22:59:15.113Z
+ * Generated on 2025-12-14T01:42:25.703Z
  * DO NOT EDIT MANUALLY
  */
 
-export const VERSION = '1.0.6009';
+export const VERSION = '1.0.6015';
 export const PACKAGE_NAME = '@continuum/jtag';
