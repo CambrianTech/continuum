f15a530c4e1118cc69c1111d855c22881622cb8b
Joel <undefined>
2025-12-28 16:11:21 -0600

    Add width: 100% to widget :host styles

--- src/debug/jtag/generator/WidgetGenerator.ts
+++ src/debug/jtag/generator/WidgetGenerator.ts
@@ -128,6 +128,7 @@ export class ${className} extends BaseWidget {
     const styles = \`
       :host {
         display: block;
+        width: 100%;
         height: 100%;
         overflow: hidden;
       }
--- src/debug/jtag/widgets/web-view/WebViewWidget.ts
+++ src/debug/jtag/widgets/web-view/WebViewWidget.ts
@@ -45,6 +45,7 @@ export class WebViewWidget extends BaseWidget {
     const styles = `
       :host {
         display: block;
+        width: 100%;
         height: 100%;
         overflow: hidden;
       }
