cdcf42744fde5a192d08c0480f94764a7f0cc054
Joel Teply <joel@cambriantech.com>
2026-09-05 13:57:19 -0500

    workspace: autosave persona work before shared sync

--- apps/web/src/render/ServingPanel.ts
+++ apps/web/src/render/ServingPanel.ts
@@ -293,23 +293,7 @@ handleGlassboxDigest(event) {
     this.requestUpdate();
   }
 
-// Handle serving.glassbox event
-connectedCallback() {
-  super.connectedCallback();
-  this.addEventListener('serving.glassbox', this.handleGlassboxDigest);
-}
 
-disconnectedCallback() {
-  super.disconnectedCallback();
-  this.removeEventListener('serving.glassbox', this.handleGlassboxDigest);
-}
-
-handleGlassboxDigest(event) {
-  if (event.type === 'serving.glassbox') {
-    this.body = event.payload;
-    this.requestUpdate();
-  }
-}
 
 // Handle serving.glassbox event
 connectedCallback() {
